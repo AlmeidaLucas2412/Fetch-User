043c656c5d09bd9cc1c0368087e69f37e98993df
Lucas <lucasprepara12@gmail.com>
2024-07-08 14:07:00 -0300

    Adicionando Modal e Favicon

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-const refreshBtn = document.querySelector('.refresh-btn')
+const head = document.querySelector('head')
+
+const backdrop = document.querySelector('.backdrop')
+const modal = document.querySelector('.modal')
+const modalBtn = document.getElementById('modal-btn')
+
+const refreshBtn = document.getElementById('refresh-btn')
 
 const imageContainer = document.querySelector('.image-container')
 
-const nameField = document.querySelector('#name')
-const emailField = document.querySelector('#email')
-const addressField = document.querySelector('#address')
-const phoneField = document.querySelector('#phone')
-const password = document.querySelector('#password')
+const nameField = document.getElementById('name')
+const emailField = document.getElementById('email')
+const addressField = document.getElementById('address')
+const phoneField = document.getElementById('phone')
+const password = document.getElementById('password')
+
+let clickCounter = 0
 
 
-
-
-function sendRequest(url) {
-  return fetch(url).then(response => response.json())
-    .catch(error => console.log(error))
+async function sendRequest(url) {
+  try {
+    const response = await fetch(url)
+    return await response.json()
+  } catch (error) {
+    return console.log(error)
+  }
 }
 
 async function fetchUser() {
@@ -31,9 +41,32 @@ async function fetchUser() {
       `
       <img class= "image" src="${src}">
       `
+    const favicon = document.createElement('link')
+    favicon.rel = 'icon'
+    favicon.type = 'image/x-icon'
+    favicon.href = src
+    // const favicon = document.createElement(`<link rel="icon" type="image/x-icon" href="${src}"`)
+    head.appendChild(favicon)
   } catch (error) {
     console.log(error)
   }
 }
 
 refreshBtn.addEventListener('click', fetchUser)
+refreshBtn.addEventListener('click', clickAccount)
+
+function clickAccount() {
+  clickCounter++
+  if(clickCounter == 10) {
+    backdrop.classList.toggle('open')
+    modal.classList.toggle('open')
+  } else if (clickCounter == 20) {
+    refreshBtn.removeEventListener('click', fetchUser)
+  }
+}
+
+modalBtn.addEventListener('click', () => {
+  backdrop.classList.toggle('open')
+  modal.classList.toggle('open')
+})
+
